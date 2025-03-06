be5856b3908ff4e7bbb5cb81df9d79e8f38d9764
vrotondo <150374510+vrotondo@users.noreply.github.com>
2025-03-05 19:02:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,12 @@
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+// Function to calculate 10% tax on a given amount
+function calculateTax(amount) {
+    const taxRate = 0.10;
+    const tax = amount * taxRate;
+    return tax;
+}
+console.log(calculateTax(amount));
+
